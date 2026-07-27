adac2593d188def8148b477565a126d37621ffab
ADMIN <adminhub.official@gmail.com>
2026-07-27 09:51:08 +0700

    Activate presence and notification update

--- _worker.js
+++ _worker.js
@@ -32,7 +32,7 @@ export default {
 
     if (url.pathname === '/' || url.pathname.endsWith('/index.html')) {
       let html = await response.text();
-      const version = '20260727-pwa-push-required-v1';
+      const version = '20260727-presence-sound-v1';
       const scripts = [
         `<script src="./app.js?v=${version}"></script>`,
         `<script src="./yayasan-dropdown-hotfix.js?v=${version}"></script>`,
