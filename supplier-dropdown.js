d0714c38736b565763570ea949d77386a34111d7
ADMIN <adminhub.official@gmail.com>
2026-07-30 13:31:49 +0700

    fix: load supplier detail and edit repair

--- supplier-dropdown.js
+++ supplier-dropdown.js
@@ -12,4 +12,5 @@
 
   loadScript('./app-dropdowns.js?v=20260730-stability-fix-v1', 'data-app-dropdowns');
   loadScript('./edit-transaction-ui.js?v=20260730-edit-form-v1', 'data-edit-transaction-ui');
+  loadScript('./supplier-actions-fix.js?v=20260730-supplier-actions-v1', 'data-supplier-actions-fix');
 })();
