3459d7593047712c07424ac2ea1a4bdb4353570c
ADMIN <adminhub.official@gmail.com>
2026-07-30 12:40:40 +0700

    chore: load consolidated form controls bundle

--- supplier-dropdown.js
+++ supplier-dropdown.js
@@ -2,7 +2,7 @@
   'use strict';
   if (document.querySelector('script[data-app-dropdowns="1"]')) return;
   var script = document.createElement('script');
-  script.src = './app-dropdowns.js?v=20260730-unified-v1';
+  script.src = './app-dropdowns.js?v=20260730-form-controls-v2';
   script.defer = true;
   script.dataset.appDropdowns = '1';
   document.head.appendChild(script);
